116c2c8cc085c79192579f58af277b0551d3a1a1
Donal OCallaghan <donal@reactivemobile.com>
2020-04-07 10:12:14 +0100

    Updating stub data

--- app/src/release/java/com/reactivemobile/app/injection/module/RatesNetworkModule.kt
+++ app/src/release/java/com/reactivemobile/app/injection/module/RatesNetworkModule.kt
@@ -4,10 +4,8 @@ import com.reactivemobile.app.data.remote.RatesNetworkService
 import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -18,14 +16,11 @@ class RatesNetworkModule {
     fun provideRetrofit(): RatesNetworkService {
         val baseUrl = "https://hiring.revolut.codes"
 
-        val interceptor =
-            HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
-        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+        val client = OkHttpClient.Builder().build()
 
         return Retrofit.Builder().baseUrl(baseUrl)
             .client(client)
-            .addConverterFactory(MoshiConverterFactory.create())
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
             .build().create(RatesNetworkService::class.java)
     }
 }
--- app/src/stub/java/com/reactivemobile/app/injection/module/RatesNetworkModule.kt
+++ app/src/stub/java/com/reactivemobile/app/injection/module/RatesNetworkModule.kt
@@ -13,14 +13,25 @@ import javax.inject.Singleton
 @Module
 class RatesNetworkModule {
 
-    private val responseString =
-        """{"baseCurrency":"EUR","rates":{"AUD":1.598,"BGN":1.979,"BRL":4.194,"CAD":1.516,"CHF":1.15,"CNY":7.762,"CZK":26.141,"DKK":7.599,"GBP":0.885,"HKD":8.982,"HRK":7.515,"HUF":318.914,"IDR":16175.183,"ILS":4.152,"INR":81.748,"ISK":135.29,"JPY":126.587,"KRW":1283.902,"MXN":21.854,"MYR":4.619,"NOK":9.76,"NZD":1.649,"PHP":59.569,"PLN":4.413,"RON":4.821,"RUB":75.203,"SEK":10.674,"SGD":1.541,"THB":35.461,"USD":1.132,"ZAR":15.929}}"""
+    private val responseStringEur =
+        """{"baseCurrency":"EUR","rates":{"AUD":1.598,"BGN":1.979}}"""
+    private val responseStringAud =
+        """{"baseCurrency":"AUD","rates":{"BGN":1.444, EUR:0.678}}"""
+    private val responseStringBgn =
+        """{"baseCurrency":"BGN","rates":{"AUD":4.554,"EUR":8.999}}"""
 
-    private val json by lazy { Gson().fromJson(responseString, RatesResponse::class.java) }
+    private val responseMap = mapOf<String, String>(
+        "EUR" to responseStringEur,
+        "AUD" to responseStringAud,
+        "BGN" to responseStringBgn
+    )
+
+    private val gson = Gson()
 
     @Provides
     @Singleton
     fun provideRetrofit() = object : RatesNetworkService {
-        override suspend fun getRates(baseCurrency: String) = json
+        override suspend fun getRates(baseCurrency: String) =
+            gson.fromJson(responseMap[baseCurrency], RatesResponse::class.java)
     }
 }
