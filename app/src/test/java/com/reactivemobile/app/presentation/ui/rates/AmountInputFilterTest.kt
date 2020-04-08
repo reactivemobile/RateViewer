5c9025ec9ae1fb1b0dd5883b4f7ac34a3df68f5b
Donal OCallaghan <donal@reactivemobile.com>
2020-04-08 12:47:56 +0100

    Extra tests on filter

--- app/src/test/java/com/reactivemobile/app/presentation/ui/rates/AmountInputFilterTest.kt
+++ app/src/test/java/com/reactivemobile/app/presentation/ui/rates/AmountInputFilterTest.kt
@@ -14,10 +14,26 @@ class AmountInputFilterTest {
     private val amountInputFilter = AmountInputFilter()
 
     @Test
-    fun filterFiltersCharactersCorrectly() {
+    fun filterFiltersSingleDecimalPointCorrectly() {
         assertEquals(
             "0.",
             amountInputFilter.filter(".", 0, 1, SpannableString(""), 0, 0)
+        )
+    }
+
+    @Test
+    fun filterFiltersMultipleDecimalPointsCorrectly() {
+        assertEquals(
+            "0.",
+            amountInputFilter.filter(".", 0, 1, SpannableString("0."), 0, 2)
+        )
+    }
+
+    @Test
+    fun filterFiltersInvalidNumberCorrectly() {
+        assertEquals(
+            "",
+            amountInputFilter.filter("0.1.2.3", 0, 6, SpannableString(""), 0, 0)
         )
     }
 }
